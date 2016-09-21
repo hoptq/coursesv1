c78172c67fc3edb0c54e3dea6ae38512faa9bac1
hoptq MBP <hoptq@topica.edu.vn>
2016-09-21 10:21:32 +0700

    New version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,41 @@
 /*jQuery*/
 $(document).ready(function() {
 
-  $('.banner_list').owlCarousel({
+  var owl = $('.course_list');
+
+  owl.owlCarousel({
     lazyLoad:true,
     loop:true,
+    nav: false,
+    margin: 10,
+    responsive:{
+        0:{
+            items:1,
+        },
+        768:{
+            items:2,
+        },
+        992:{
+            items:3,
+        },
+        1366:{
+            items:5,
+        },
+        1600: {
+          items: 6,
+        },
+        1920: {
+          items: 7,
+        }
+    }
+  });
+
+  $('.banner_list').owlCarousel({
+    loop:true,
     margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
+    autoPlay:true,
+    autoPlayTimeout:3000,
+    autoPlayHoverPause:true,
     nav: false,
     responsive:{
         0:{
@@ -27,60 +55,29 @@ $(document).ready(function() {
         }
     }
   });
-  $('.course_list').owlCarousel({
-    lazyLoad:true,
-    loop:true,
-    nav: false,
-    responsive:{
-        0:{
-            items:1,
-            margin:10,
-        },
-        544:{
-            items:2,
-            margin:10
-        },
-        768:{
-            items:3,
-            margin: 10
-        },
-        992:{
-            items:4,
-            margin: 10
-        },
-        1366:{
-            items:5,
-            margin: 10
-        },
-        1600: {
-          items: 6,
-          margin: 10
-        },
-        1920: {
-          items: 7,
-          margin: 10
-        }
-    }
-  });
-  var owlWrap = $('.owl-wrapper');
-  if (owlWrap.length > 0) {
-    if (jQuery().owlCarousel) {
-      owlWrap.each(function(){
-          var carousel= $(this).find('.owl-carousel'),
-              navigation = $(this).find('.customNavigation'),
-              nextBtn = navigation.find('.next'),
-              prevBtn = navigation.find('.prev');
-       
-        nextBtn.click(function(){
-          console.log('click next');
-          carousel.trigger('next.owl');
-        });
-        prevBtn.click(function(){
-          console.log('click prev');
-          carousel.trigger('prev.owl');
-        });
 
-      });
+  var owlWrap = $('.owl-wrapper');
+    // checking if the dom element exists
+    if (owlWrap.length > 0) {
+        // check if plugin is loaded
+        if (jQuery().owlCarousel) {
+            owlWrap.each(function(){
+                var carousel= $(this).find('.owl-carousel'),
+                    navigation = $(this).find('.customNavigation'),
+                    nextBtn = navigation.find('.next'),
+                    prevBtn = navigation.find('.prev');
+
+              carousel.owlCarousel();
+             
+              // Custom Navigation Events
+              nextBtn.click(function(e){
+                carousel.trigger('next.owl.carousel');
+              });
+              prevBtn.click(function(){
+                carousel.trigger('prev.owl.carousel');
+              });
+
+            });
+        };
     };
-  };
 });
